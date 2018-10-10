6d3ae3811110361c8b96ab994587ddc47d9cc2a1
Michel Bongard <michel_bongard@hotmail.com>
2018-10-10 17:48:30 +0200

    added getters and setters to class Beer.java

--- app/src/main/java/ch/beerpro/domain/models/Beer.java
+++ app/src/main/java/ch/beerpro/domain/models/Beer.java
@@ -24,4 +24,61 @@ public class Beer implements Entity, Serializable {
     private float avgRating;
     private int numRatings;
 
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public float getAvgRating() {
+        return avgRating;
+    }
+
+    public void setAvgRating(float avgRating) {
+        this.avgRating = avgRating;
+    }
+
+    public int getNumRatings() {
+        return numRatings;
+    }
+
+    public void setNumRatings(int numRatings) {
+        this.numRatings = numRatings;
+    }
 }
