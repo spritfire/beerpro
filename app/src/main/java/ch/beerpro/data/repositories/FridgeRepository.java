786086eb230cd8cdb41382b455524a2480f4f0eb
Michel Bongard <michel_bongard@hotmail.com>
2018-10-14 20:10:00 +0200

    implemented fridge repository

---
+++ app/src/main/java/ch/beerpro/data/repositories/FridgeRepository.java
@@ -0,0 +1,74 @@
+package ch.beerpro.data.repositories;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import ch.beerpro.domain.models.FridgeItem;
+import ch.beerpro.domain.utils.FirestoreQueryLiveDataArray;
+
+public class FridgeRepository {
+
+    private static LiveData<List<FridgeItem>> getFridgeByUser(String userId) {
+        return new FirestoreQueryLiveDataArray<>(FirebaseFirestore.getInstance().collection(FridgeItem.COLLECTION)
+                .orderBy(FridgeItem.FIELD_AMOUNT, Query.Direction.ASCENDING).whereEqualTo(FridgeItem.FIELD_USER_ID, userId),
+                FridgeItem.class);
+    }
+
+
+    public Task<Void> toggleUserFridgeItem(String userId, String itemId) {
+        DocumentReference fridgeEntryQuery = getFridgeItemEntry(userId, itemId);
+        return fridgeEntryQuery.get().continueWithTask(task -> {
+            if (task.isSuccessful() && task.getResult().exists()) {
+                return fridgeEntryQuery.delete();
+            } else if (task.isSuccessful()) {
+                return fridgeEntryQuery.set(new FridgeItem(userId, itemId, 1));
+            } else {
+                throw task.getException();
+            }
+        });
+    }
+
+    public void increaseFridgeItemAmount(String userId, String itemId) {
+        DocumentReference fridgeEntryQuery = getFridgeItemEntry(userId, itemId);
+        fridgeEntryQuery.get().continueWithTask(task -> {
+            if (task.isSuccessful() && task.getResult().exists()) {
+                FridgeItem fridgeItem = fridgeEntryQuery.get().getResult().toObject(FridgeItem.class);
+                int newAmount = fridgeItem.getAmount() + 1;
+                fridgeEntryQuery.update(FridgeItem.FIELD_AMOUNT, newAmount);
+                return fridgeEntryQuery.delete();
+            } else {
+                throw task.getException();
+            }
+        });
+    }
+
+    public void decreaseFridgeItemAmount(String userId, String itemId) {
+        DocumentReference fridgeEntryQuery = getFridgeItemEntry(userId, itemId);
+        fridgeEntryQuery.get().continueWithTask(task -> {
+            if (task.isSuccessful() && task.getResult().exists()) {
+                FridgeItem fridgeItem = fridgeEntryQuery.get().getResult().toObject(FridgeItem.class);
+                int newAmount = fridgeItem.getAmount() - 1;
+                if (newAmount > 0) {
+                    fridgeEntryQuery.update(FridgeItem.FIELD_AMOUNT, newAmount);
+                } else {
+                    fridgeEntryQuery.delete();
+                }
+                return null;
+            } else {
+                throw task.getException();
+            }
+        });
+    }
+
+    private DocumentReference getFridgeItemEntry(String userId, String itemId) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        String fridgeItemId = FridgeItem.generateId(userId, itemId);
+        return db.collection(FridgeItem.COLLECTION).document(fridgeItemId);
+    }
+
+}
---
+++ app/src/main/java/ch/beerpro/domain/models/FridgeItem.java
@@ -0,0 +1,53 @@
+package ch.beerpro.domain.models;
+
+import com.google.firebase.firestore.Exclude;
+
+import lombok.NonNull;
+
+public class FridgeItem implements Entity {
+    public static final String COLLECTION = "fridgeitems";
+    public static final String FIELD_ID = "id";
+    public static final String FIELD_USER_ID = "userId";
+    public static final String FIELD_BEER_ID = "beerId";
+    public static final String FIELD_AMOUNT = "amount";
+
+    /**
+     * The id is formed by `$userId_$beerId` to make queries easier.
+     */
+    @Exclude
+    private String id;
+    @NonNull
+    private String userId;
+    @NonNull
+    private String beerId;
+    @NonNull
+    private int amount;
+
+    public FridgeItem(String userId, String beerId, int amount) {
+        this.userId = userId;
+        this.beerId = beerId;
+        this.amount = amount;
+    }
+
+    public static String generateId(String userId, String beerId) {
+        return String.format("%s_%s", userId, beerId);
+    }
+
+    @Override
+    public String getId() { return id; }
+
+    @Override
+    public void setId(String id) { this.id = id; }
+
+    public String getUserId() { return userId; }
+
+    public void setUserId(String userId) { this.userId = userId; }
+
+    public String getBeerId() { return beerId; }
+
+    public void setBeerId(String beerId) { this.beerId = beerId; }
+
+    public int getAmount() { return amount; }
+
+    public void setAmount(int amount) { this.amount = amount; }
+}
